c911c8d131404b862c9215fbdba95ea03c5e6a18
Ohans Emmanuel <ohansemmanuel@gmail.com>
2019-04-23 07:09:32 +0200

    points app cleanup

--- 01-New-Lifecycle-Methods/01-points/src/App.js
+++ 01-New-Lifecycle-Methods/01-points/src/App.js
@@ -1,77 +1,83 @@
-import React, { Component } from "react";
-import logo from "./logo.png";
-import "./App.css";
+import React, { Component } from 'react'
+import logo from './logo.png'
+import './App.css'
 
 class Chats extends Component {
-  render() {
+  render () {
     return (
       <React.Fragment>
         {this.props.chatList.map((chat, i) => {
           if (i === 4) {
             // Simulate an error
-            throw new Error("I crashed!");
+            throw new Error('I crashed!')
           }
           return (
-            <li key={i} className="chat-bubble">
+            <li key={i} className='chat-bubble'>
               {chat}
             </li>
-          );
+          )
         })}
       </React.Fragment>
-    );
+    )
   }
 }
 
 class App extends Component {
-  chatThreadRef = React.createRef();
+  chatThreadRef = React.createRef()
 
+  // NB: unconditionally overriding state here is generally a bad idea.
+  static getDerivedStateFromProps (props, state) {
+    return {
+      points: 1000
+    }
+  }
   state = {
     points: 10,
-    chatList: ["Hey", "Hello", "Hi"]
-  };
+    chatList: ['Hey', 'Hello', 'Hi']
+  }
 
   _handleAddChat = () => {
     this.setState(prevState => ({
-      chatList: [...prevState.chatList, "Hello!!!"]
-    }));
-  };
+      chatList: [...prevState.chatList, 'Hello!!!']
+    }))
+  }
 
-  getSnapshotBeforeUpdate(prevProps, prevState) {
+  getSnapshotBeforeUpdate (prevProps, prevState) {
     if (this.state.chatList > prevState.chatList) {
-      const chatThreadRef = this.chatThreadRef.current;
-      return chatThreadRef.scrollHeight - chatThreadRef.scrollTop;
+      const chatThreadRef = this.chatThreadRef.current
+      return chatThreadRef.scrollHeight - chatThreadRef.scrollTop
     }
-    return null;
+    return null
   }
 
-  componentDidUpdate(prevProps, prevState, snapshot) {
+  componentDidUpdate (prevProps, prevState, snapshot) {
     if (snapshot !== null) {
-      const chatThreadRef = this.chatThreadRef.current;
-      chatThreadRef.scrollTop = chatThreadRef.scrollHeight - snapshot;
+      const chatThreadRef = this.chatThreadRef.current
+      chatThreadRef.scrollTop = chatThreadRef.scrollHeight - snapshot
     }
   }
 
-  render() {
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
           <p>You've scored {this.state.points} points.</p>
         </header>
-        <section className="App-chat">
-          <p className="chat-header">
-            Football Chat{" "}
-            <button className="chat-btn" onClick={this._handleAddChat}>
+        <section className='App-chat'>
+          <p className='chat-header'>
+            Football Chat{' '}
+            <button className='chat-btn' onClick={this._handleAddChat}>
               Add Chat
             </button>
           </p>
-          <ul className="chat-thread" ref={this.chatThreadRef}>
+          <ul className='chat-thread' ref={this.chatThreadRef}>
             <Chats chatList={this.state.chatList} />
           </ul>
         </section>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
